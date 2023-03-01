dbad234f7425024b0b6c6eb9cb6ace89709b031f
Alice Troussier <alicedu05@hotmail.fr>
2023-03-01 14:37:50 +0100

    depot initial

---
+++ terre00.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+#tab = ["a", "b", "c", "d", "e", "f","g"]
+tab = ["a""b""c""d""e""f""g""h""i""j""k""l""m""n""o""p""q""r""s""t""u""v""w""x""y""z"]
+
+for c in tab[0..-1] #c pour chaine de caractère
+  print "#{c}\n"# Instruction(s).
+end
+
+
+
