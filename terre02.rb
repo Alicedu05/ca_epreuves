73a9ea27589c26e34a96b69bb92d2cada87cf5d6
Alice Troussier <alicedu05@hotmail.fr>
2023-03-03 17:50:48 +0100

    ajout des exos 01 02

---
+++ terre02.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+#Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
+
+#Exemples d’utilisation :
+#$> ruby exo.rb je suis solide !
+#je
+#suis
+#solide
+#!
+
+montexte = "je suis motivée et rien ne m'arrete ! ".split #split decoupe montexe, et le separateur est un espace
+puts montexte
