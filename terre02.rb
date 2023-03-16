d96939a9f6c21636e26f1f4358ea567f9cf8dcc7
Alice Troussier <alicedu05@hotmail.fr>
2023-03-16 10:37:57 +0100

    ajout terre02

--- terre02.rb
+++ terre02.rb
@@ -1,13 +1,19 @@
 #!/usr/bin/env ruby
 
 #Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
-
 #Exemples d’utilisation :
+
 #$> ruby exo.rb je suis solide !
 #je
 #suis
 #solide
-#!
 
-montexte = "je suis motivée et rien ne m'arrete ! ".split #split decoupe montexe, et le separateur est un espace
-puts montexte
+puts ARGV
+
+# les truc que j'ai essayer avant de trouver la simplicitée ....
+#monTexte = ARGV.split
+#puts "#{monTexte}"
+#for i in monTexte(0..-1)
+  #print "#{i}\n"
+#end
+
