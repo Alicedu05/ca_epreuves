bb06150d475695a57fe0fae2e2a8c5dd9457012e
Alice Troussier <alicedu05@hotmail.fr>
2023-04-07 15:14:57 +0200

    nettoyage_du_code

--- terre09.rb
+++ terre09.rb
@@ -6,14 +6,6 @@
 #3
 #Attention : je compte sur vous pour gérer les potentielles erreurs d’arguments
 
-#Racine carrée d’un nombre
-
-#Créez un programme qui affiche la racine carrée d’un entier positif.
-#Exemples d’utilisation :
-#$> node exo.js 9
-#3
-#Attention : je compte sur vous pour gérer les potentielles erreurs d’arguments
-
 c = ARGV[0].to_i #entier
 resultat = 0
 
