e64271e6aaf94aa03fc796673a4cb08d9d3716f8
Alice Troussier <alicedu05@hotmail.fr>
2023-04-28 12:15:18 +0200

    amelioration

--- terre10.rb
+++ terre10.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 #Nombre premier
 
 #Créez un programme qui affiche si un nombre est premier ou pas.
@@ -10,30 +9,33 @@
 #Attention : 0 et 1 ne sont pas des nombres premiers. Gérez les erreurs d’arguments.
 
 chiffre_argument = ARGV[0].to_f
-racine_chiffre = chiffre_argument**(1.0/2) #racine
-#puts r
+racine_chiffre = chiffre_argument**(1.0/2)
+
 if ARGV.empty?
         puts " *** Vous n'avez pas saisi d'arguments *** "
 elsif ARGV.size > 1
         puts " *** Vous n'avez trop d'arguments *** "
 elsif ARGV[0].match(/[a-zA-Z]/)
         puts " *** Vous devez saisir un chiffre en d'arguments non une lettre *** "
-elsif chiffre_argument < 2
-        puts " *** Attention, l'argument doit etre un chiffre positif et supperieur à 1 *** "
-elsif racine_chiffre <= 1.99 #pas foufou
-                puts "Oui, #{chiffre_argument.to_i} est un nombre premier."
-                exit
+elsif chiffre_argument <= 0
+        puts "Non, #{chiffre_argument.to_i} n’est pas un nombre premier."
+elsif chiffre_argument <= 1
+        puts  "Non, #{chiffre_argument.to_i} n’est pas un nombre premier."
+elsif racine_chiffre > 1 && racine_chiffre < 2
+        puts "Oui, #{chiffre_argument.to_i} est un nombre premier."
 else
+   est_premier = true
     (2..racine_chiffre).each do |i|
-           if chiffre_argument % i == 0
-                puts "Non, #{chiffre_argument.to_i} n’est pas un nombre premier."
-                exit
-            else
-                puts "Oui, #{chiffre_argument.to_i} est un nombre premier."
-                exit
-            end
+        if chiffre_argument % i == 0
+            est_premier = false
+            break
+        end
+    end
+
+    if est_premier == true
+        puts "Oui, #{chiffre_argument.to_i} est un nombre premier."
+    else
+        puts "Non, #{chiffre_argument.to_i} n’est pas un nombre premier."
     end
 end
-
-
 
