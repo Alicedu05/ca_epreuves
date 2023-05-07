22a7d64c878634e58e365f1eade8aac98ef95612
Alice Troussier <alicedu05@hotmail.fr>
2023-05-07 11:55:11 +0200

    Fin_de_la_terre

---
+++ terre.rb
@@ -0,0 +1,10 @@
+#Créez un programme qui célèbre votre victoire.
+
+#Exemples d’utilisation :
+#$> ruby exo.rb
+#J’ai terminé l’Épreuve de la Terre et c’était [].
+#$>
+
+#Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+
+puts "J’ai terminé l’Épreuve de la Terre et c’était pas facile touts les jours. J'ai pris mon temps pour faire tout par moi même, et je suis fière du chemin accomplie. Je suis prête et motivée pour la suite."
